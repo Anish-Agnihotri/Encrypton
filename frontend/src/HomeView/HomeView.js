e96697a862f7135767848a6f6bfdb7f9ba36f13a
anish-agnihotri <contact@anishagnihotri.com>
2018-09-16 09:24:05 -0400

    Adding favicon

--- frontend/src/HomeView/HomeView.js
+++ frontend/src/HomeView/HomeView.js
@@ -4,8 +4,42 @@ import './HomeView.css';
 class HomeView extends Component {
     render () {
         return (
-           <div className="leftSide">
-                <p>something</p>
+           <div>
+               <div className="leftSide">
+                    <img src="https://i.imgur.com/qvn0Xu0.png" />
+                    <h2><strong>Trusted</strong> documents on a <strong>trustless</strong> ledger.</h2>
+                    <p>Encrypton brings together the best of both worlds, connecting the public, transparent power of the Ethereum-IPFS platform and the security of a trustless platform for document encryption, right at your fingertips.</p>
+                </div>
+                <div className="rightSide">
+                    <div class='wrapper'>
+                        <div class='container'>
+                            <div class='sphere'>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            </div>
+                            <div class='sphere'>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            <div class='ring'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
            </div>
         );
     }
