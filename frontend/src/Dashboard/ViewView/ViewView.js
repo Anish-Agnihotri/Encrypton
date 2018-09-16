1a296d428d71e9f19925194256f2e4d918db5bb4
anish-agnihotri <contact@anishagnihotri.com>
2018-09-15 23:15:00 -0400

    Added view document screen

--- frontend/src/Dashboard/ViewView/ViewView.js
+++ frontend/src/Dashboard/ViewView/ViewView.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './ViewView.css';
 
 class ViewView extends Component {
-    render () {
+    render() {
         return (
-            <p>remove this crap</p>
-        );
-    }
-}
-
+            <div className="marginPadding">
+                <div className="card bg-light mb-3 mx-auto" id="checkHash">
+                    <div className="card-header">
+                        <b>Step 1:</b> Enter your hash and decryption key.
+                    </div>
+                    <div className="card-body addToCard">
+                        <div className="form-group form-inline">
+                            <input type="text" placeholder="Input your file hash." className="form-control" />
+                            <input type="password" placeholder="Input your decryption key." className="form-control" />
+                            <button className="textSubmit set btn btn-outline-primary">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                );
+            }
+        }
+        
 export default ViewView;
